772c55b87eb32e542d2c263e5bbb58400aa82a5f
Wes Ward <ward920@gmail.com>
2021-01-14 21:51:25 +0000

    t

--- main.tf
+++ main.tf
@@ -193,8 +193,3 @@ resource "aws_key_pair" "hashicat" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.hashicat.public_key_openssh
 }
-
-mandatory_tags = [
-  ,Department = "devops"
-  ,Billable = "true"
-]
